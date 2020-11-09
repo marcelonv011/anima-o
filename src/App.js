11449189cd4a6d96b38b60067111927e10904b4a
marcelo <marcelonv011@gmail.com>
2020-11-08 22:58:36 -0300

    trabajo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+            
+      <h1 className= "titulo">marcelo login
+      <main>
+   <div class="dank-ass-loader">
+      <div class="row">
+         <div class="arrow up outer outer-18"></div>
+         <div class="arrow down outer outer-17"></div>
+         <div class="arrow up outer outer-16"></div>
+         <div class="arrow down outer outer-15"></div>
+         <div class="arrow up outer outer-14"></div>
+      </div>
+      <div class="row">
+         <div class="arrow up outer outer-1"></div>
+         <div class="arrow down outer outer-2"></div>
+         <div class="arrow up inner inner-6"></div>
+         <div class="arrow down inner inner-5"></div>
+         <div class="arrow up inner inner-4"></div>
+         <div class="arrow down outer outer-13"></div>
+         <div class="arrow up outer outer-12"></div>
+      </div>
+      <div class="row">
+         <div class="arrow down outer outer-3"></div>
+         <div class="arrow up outer outer-4"></div>
+         <div class="arrow down inner inner-1"></div>
+         <div class="arrow up inner inner-2"></div>
+         <div class="arrow down inner inner-3"></div>
+         <div class="arrow up outer outer-11"></div>
+         <div class="arrow down outer outer-10"></div>
+      </div>
+      <div class="row">
+         <div class="arrow down outer outer-5"></div>
+         <div class="arrow up outer outer-6"></div>
+         <div class="arrow down outer outer-7"></div>
+         <div class="arrow up outer outer-8"></div>
+         <div class="arrow down outer outer-9"></div>
+      </div>
+   </div>
+</main>
+</h1>
+    <div className="log">
+  <form className="usuario">
+    <label>
+      Name:
+      <input type="text" name="name" />
+      <input type="submit" value="Submit" />
+    </label>
+    <div className="app"></div>
+    <label>
+      password:
+      <input type="text" name="password" />
+    </label>
+  <input type="submit" value="Submit" />
+  </form>
+    </div>
     </div>
   );
 }
